7303c3edba3c4ccb7af3b410e2e0879cbb50df62
luciennep <luciennepereiraananias@gmail.com>
2026-06-07 00:14:34 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   const { code } = req.query;
   const response = await fetch('https://github.com/login/oauth/access_token', {
     method: 'POST',
