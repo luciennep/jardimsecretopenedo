43fedd47d5fe4ec62eee1367a137708555777547
luciennep <luciennepereiraananias@gmail.com>
2026-06-07 00:12:44 -0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   const params = new URLSearchParams({
     client_id: process.env.GITHUB_CLIENT_ID,
     scope: 'repo,user',
