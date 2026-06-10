bd8848e734af92f905568a2d59fb469b006d8b4d
luciennep <luciennepereiraananias@gmail.com>
2026-06-09 21:42:06 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,25 +1,55 @@
 export default async function handler(req, res) {
   const { code } = req.query;
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    body: JSON.stringify({
-      client_id: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_CLIENT_SECRET,
-      code,
-    }),
-  });
-  const data = await response.json();
-  const token = data.access_token;
-  res.send(`
-    <script>
-      (function() {
-        window.opener.postMessage(
-          'authorization:github:success:{"token":"${token}","provider":"github"}',
-          '*'
-        );
-        window.close();
-      })();
-    </script>
-  `);
+
+  if (!code) {
+    return res.status(400).send("Erro: código de autorização não recebido.");
+  }
+
+  try {
+    const response = await fetch("https://github.com/login/oauth/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!data.access_token) {
+      return res.status(500).send(`
+        <h1>Erro ao autenticar com GitHub</h1>
+        <pre>${JSON.stringify(data, null, 2)}</pre>
+      `);
+    }
+
+    res.setHeader("Content-Type", "text/html");
+    return res.send(`
+      <!doctype html>
+      <html>
+        <body>
+          <script>
+            window.opener.postMessage(
+              'authorization:github:success:${JSON.stringify({
+                token: data.access_token,
+                provider: "github"
+              }).replace(/'/g, "\\'")}',
+              window.location.origin
+            );
+            window.close();
+          </script>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    return res.status(500).send(`
+      <h1>Erro interno no callback</h1>
+      <pre>${error.message}</pre>
+    `);
+  }
 }
